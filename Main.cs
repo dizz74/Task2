b630e34048392e3d1691a501dc6af36c2d30766e
dizz74 <dizz74@mail.ru>
2022-03-06 23:15:41 +0500

    f

--- Main.cs
+++ Main.cs
@@ -1,5 +1,6 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 using System;
@@ -18,29 +19,65 @@ namespace Task2
             UIApplication uiApp = commandData.Application;
             UIDocument uiDocument = uiApp.ActiveUIDocument;
             Document doc = uiDocument.Document;
-            Group group;
-            XYZ point;
+             
+        
             try
             {
                 var refGroup = uiDocument.Selection.PickObject(ObjectType.Element, new GroupFilter(), "Выберите группу");
-                  group = doc.GetElement(refGroup) as Group;
-                  point = uiDocument.Selection.PickPoint("Выберите точку вставки");
+                Group group = doc.GetElement(refGroup) as Group;
+                XYZ groupCenter = GetElementCenter(group);
+                Room room = GetRoomByPoint(doc, groupCenter);
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = groupCenter - roomCenter;
+                XYZ newPointroom = uiDocument.Selection.PickPoint("Выберите точку внутри комнаты");
+
+                Room selectedRoom = GetRoomByPoint(doc, newPointroom);
+                XYZ selectedRoomCenter = GetElementCenter(selectedRoom);
+
+
+                XYZ newCenterPoint = selectedRoomCenter + offset;
+                using (var ts = new Transaction(doc, "copy group"))
+                {
+                    ts.Start();
+                    doc.Create.PlaceGroup(newCenterPoint, group.GroupType);
+                    ts.Commit();
+                }
             }
-            catch (Autodesk.Revit.Exceptions.OperationCanceledException e) {
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException e)
+            {
                 return Result.Cancelled;
             }
-            
-            using (var ts = new Transaction(doc, "copy group"))
-            {
-                ts.Start();
-                doc.Create.PlaceGroup(point, group.GroupType);
-                ts.Commit();
+            catch (Exception e) {
+                message = e.Message;
+                return Result.Failed;
             }
+
+            
 
 
             return Result.Succeeded;
         }
 
+
+        private Room GetRoomByPoint(Document doc, XYZ xyz)
+        {
+            FilteredElementCollector elements = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element element in elements)
+            {
+                Room room = element as Room;
+                if (room != null)
+                {
+                    if (room.IsPointInRoom(xyz)) return room;
+
+                }
+            }
+            return null;
+        }
+        private XYZ GetElementCenter(Element element)
+        {
+            var bounding = element.get_BoundingBox(null);
+            return (bounding.Max + bounding.Min) / 2;
+        }
         private class GroupFilter : ISelectionFilter
         {
             public bool AllowElement(Element elem)
